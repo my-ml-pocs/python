9005224445ac748b327930a3de815894d164f6b6
ajepam <ankit_jain@epam.com>
2025-03-17 10:50:46 -0700

    Update tests/test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -40,6 +40,7 @@ from tests.factories import ProductFactory
 logging.disable(logging.CRITICAL)
 
 # DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///../db/test.db')
+# Removed.
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
